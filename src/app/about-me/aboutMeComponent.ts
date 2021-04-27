f754b5075145e00acf80dd14068cd1617332527e
Antoine de Villers <antoinedevillers0307@gmail.com>
2021-04-27 18:13:58 -0400

    Ajout de Contacter moi et francisation

--- src/app/about-me/aboutMeComponent.ts
+++ src/app/about-me/aboutMeComponent.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
   name: string;
+  description: string;
 
   constructor() {
     this.name = 'Antoine de Villers';
+    this.description = "Bonjour, je suis un développeur web Angular. "
   }
 
   ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutMeComponent } from './about-me/aboutMeComponent';
 import { HeaderComponent } from './header/headerComponent';
 import { NavComponent } from './nav/navComponent';
 import { ContentAreaComponent } from './contentArea/contentAreaComponent';
+import { AboutMeComponent } from './about-me/aboutMeComponent';
+import { ContactComponent } from './contact/contactComponent';
 import { FooterComponent } from './footer/footerComponent';
 
 
@@ -15,10 +16,11 @@ import { FooterComponent } from './footer/footerComponent';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutMeComponent,
     HeaderComponent,
     NavComponent,
     ContentAreaComponent,
+    AboutMeComponent,
+    ContactComponent,
     FooterComponent
   ],
   imports: [
