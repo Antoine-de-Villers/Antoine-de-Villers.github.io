e21414f2f524fde3da1e9b125dcaf73148fa75cf
Antoine de Villers <antoinedevillers0307@gmail.com>
2022-04-01 21:04:01 -0400

    Update du About-me

--- src/app/about-me/aboutMeComponent.ts
+++ src/app/about-me/aboutMeComponent.ts
@@ -10,7 +10,7 @@ export class AboutMeComponent implements OnInit {
 
   constructor() {
     this.name = 'Antoine de Villers';
-    this.description = "Bonjour, je suis un développeur web Angular. "
+    this.description = "Bonjour, je suis un analyste en Assurance Qualité Logicielle avec un background en développement web Angular. "
   }
 
   ngOnInit(): void {
